5961adab571862acd1f43abc2f28521622fb1ab0
isabellaTeixeir <teixeiraisabela665@gmail.com>
2024-09-12 11:48:15 -0300

    Add files via upload

---
+++ login/default.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <?php
+    echo "Primeiro PHP!<br>";
+    $nome = "Gabriel";
+    $Sobrenome = "Kreischer";
+    echo "$nome"," $Sobrenome";
+    ?>
+</body>
+</html>
